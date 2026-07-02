98110b68bd737fe2d2274354e315490312ccb87d
brunopalmeirascruz-hue <bruno.palmeiras.cruz@escola.pr.gov.br>
2026-07-02 16:02:25 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function mostrarMensagem(){
-    document.getElementById("mensagem").innerHTML =
-    "Esta notícia é fictícia e foi criada apenas para demonstrar como uma fake news pode ser apresentada. Sempre confirme informações em fontes oficiais.";
-}
